623b0cec98d8d65738ff5501e0923c41e512a945
_Zoli <zoli1984@gmail.hu>
2023-06-18 19:45:06 +0200

    modal doesnť working

--- js/kedvencek.js
+++ js/kedvencek.js
@@ -1,13 +1,38 @@
-//MDB Lightbox Init
-$(function () {
-$("#mdb-lightbox-ui").load("mdb-addons/mdb-lightbox-ui.html");
-});
+function openModal() {
+    document.getElementById("myModal").style.display = "block";
+}
 
+function closeModal() {
+    document.getElementById("myModal").style.display = "none";
+}
 
-/*$(document).ready(function(){
-    $('.mozgokepek').animate({
-        marginTop: "-=50px", 
-    },1000);
-});
-*/
+var slideIndex = 1;
+showSlides(slideIndex);
 
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("demo");
+    var captionText = document.getElementById("caption");
+    if (n > slides.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = slides.length }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+    captionText.innerHTML = dots[slideIndex - 1].alt;
+}
+
+console.log("Az openModal függvény meghívódott!");
